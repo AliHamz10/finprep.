8b9e159b49793745bce8fb38e537025f1e0077f8
Ali Hamza <148047346+AliHamz10@users.noreply.github.com>
2025-04-03 03:40:01 +0500

    Header Allignment

--- components/header.jsx
+++ components/header.jsx
@@ -1,16 +1,26 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
-const header = () => {
+const Header = () => {
   return (
-    <div>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </div>
+    <header className="fixed top-0 flex justify-between items-center bg-[#111828] text-white px-6 py-3 shadow-md">
+      {/* Logo */}
+      <div className="flex items-center">
+        <img
+          src="/icons/finprep.svg"
+          alt="Finprep Logo"
+          className="w-24 h-auto" // Reduced size
+        />
+      </div>
+
+      {/* Authentication Buttons */}
+      <div>
+        <SignedOut>
+          <SignInButton></SignInButton>
+        </SignedOut>
+        <SignedIn></SignedIn>
+      </div>
+    </header>
   );
 };
 
-export default header;
+export default Header;
