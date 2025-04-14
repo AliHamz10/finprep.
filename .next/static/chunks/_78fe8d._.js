ea63a2289d551338aff5afbbde471bf9318a4f41
Ali Hamza <148047346+AliHamz10@users.noreply.github.com>
2025-04-14 12:37:03 +0500

    Complete Project done

--- .next/static/chunks/_78fe8d._.js
+++ .next/static/chunks/_78fe8d._.js
@@ -206,7 +206,7 @@ const HeroSection = ()=>{
                         ref: imageRef,
                         className: "hero-image",
                         children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$image$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["default"], {
-                            src: "/banner.jpeg",
+                            src: "/banner.png",
                             width: 1280,
                             height: 720,
                             alt: "Dashboard Preview",
